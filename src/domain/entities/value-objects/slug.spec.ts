1a2f30b6f3c3afabb3e660593581f04db422b2c3
Agleylson Melo <agleylsonmelo@outlook.com>
2024-07-05 20:13:19 -0300

    DDD Fundamentals - Path aliases and Vitest globals

--- src/domain/entities/value-objects/slug.spec.ts
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -2,7 +2,7 @@ import { expect, it } from "vitest";
 import { Slug } from "./slug";
 
 it("should be able to create a new slug from text", () => {
-  const slug = Slug.createFromTex("Example slug question title")
+  const slug = Slug.createFromText("Example slug question title")
 
   expect(slug.value).toEqual("example-slug-question-title")
 })

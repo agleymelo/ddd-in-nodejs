8418a92c40c62e9547da4f854267b3fd9c233787
Agleylson Melo <agleylsonmelo@outlook.com>
2024-07-05 19:40:19 -0300

    DDD Fundamentals - Entity ID

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,15 +1,15 @@
-import { randomUUID } from "node:crypto"
+import { UniqueEntityID } from './unique-entity-id'
 
 export class Entity<Props> {
-  private _id: string
+  private _id: UniqueEntityID
   protected props: Props
+
+  constructor(props: Props, id?: string) {
+    this._id = new UniqueEntityID(id)
+    this.props = props
+  }
 
   get id() {
     return this._id
-  }
-
-  constructor(props: Props, id?: string) {
-    this._id = id ?? randomUUID()
-    this.props = props
   }
 }

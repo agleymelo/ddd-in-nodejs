d21f52c03f1e7b51069c53be4444e206dc7fa1f8
Agleylson Melo <agleylsonmelo@outlook.com>
2024-07-06 20:01:06 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Buscar pergunta pelo slug

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -0,0 +1,28 @@
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface GetQuestionBySlugUseCaseRequest {
+  slug: string
+}
+
+interface GetQuestionBySlugUseCaseReply {
+  question: Question
+}
+
+export class GetQuestionBySlugUseCase {
+  constructor(private questionRepository: QuestionsRepository) { }
+
+  async execute({
+    slug,
+  }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseReply> {
+    const question = await this.questionRepository.findBySlug(slug)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    return {
+      question,
+    }
+  }
+}

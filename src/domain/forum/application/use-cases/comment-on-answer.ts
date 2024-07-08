e78a78fea0ea2355f83c7fbf3189d457dc234be1
Agleylson Melo <agleylsonmelo@outlook.com>
2024-07-08 18:22:33 -0300

    Forum with DDD and Clean Architecture - Use case: Comment on the answer

---
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -0,0 +1,45 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { AnswerCommentsRepository } from '../repositories/answers-comment-repository'
+
+interface CommentOnAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+  content: string
+}
+
+interface CommentOnAnswerUseCaseReply {
+  answerComment: AnswerComment
+}
+
+export class CommentOnAnswerUseCase {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private answerCommentsRepository: AnswerCommentsRepository,
+  ) { }
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+  }: CommentOnAnswerUseCaseRequest): Promise<CommentOnAnswerUseCaseReply> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    const answerComment = AnswerComment.create({
+      authorId: new UniqueEntityID(authorId),
+      answerId: new UniqueEntityID(answerId),
+      content,
+    })
+
+    await this.answerCommentsRepository.create(answerComment)
+
+    return {
+      answerComment,
+    }
+  }
+}
--- test/repositories/in-memory-question-comment-repository.ts
+++ test/repositories/in-memory-question-comment-repository.ts
@@ -1,12 +1,11 @@
-import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
-import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
 export class InMemoryQuestionCommentsRepository
-  implements QuestionCommentsRepository
-{
-  public items: QuestionComment[] = [];
+  implements QuestionCommentsRepository {
+  public items: QuestionComment[] = []
 
   async create(questionComment: QuestionComment): Promise<void> {
-    this.items.push(questionComment);
+    this.items.push(questionComment)
   }
 }
